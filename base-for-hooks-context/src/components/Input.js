1f56eaece566926d10be1ea5647d7b37bd4e2323
Angelo Zambrano <angelozam17@gmail.com>
2020-12-06 18:58:42 -0500

    feat(input): add logic and styles

--- base-for-hooks-context/src/components/Input.js
+++ base-for-hooks-context/src/components/Input.js
@@ -1,7 +1,62 @@
-import React from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
 
-function Input() {
-  return <div data-test="input-container" />;
+// Testing
+export const dataAttrs = {
+  container: "input-container",
+  form: "input-form",
+  input: "input-box",
+  button: "input-submit-button",
+};
+
+// Component
+function Input({ secretWord }) {
+  // States
+  const [currentGuessWord, setCurrentGuessWord] = React.useState(null);
+
+  // Methods
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // TODO: update guessedWord
+    // TODO: check against secretWord and update success
+    setCurrentGuessWord("");
+  };
+
+  const handleChange = (e) => {
+    setCurrentGuessWord(e.target.value);
+  };
+
+  return (
+    <div data-test={dataAttrs.container}>
+      <form
+        data-test={dataAttrs.form}
+        onSubmit={handleSubmit}
+        className="text-center"
+      >
+        <input
+          className="py-1 px-3 border outline-none focus:border-green-400"
+          type="text"
+          placeholder="Enter guess"
+          data-test={dataAttrs.input}
+          value={currentGuessWord}
+          onChange={handleChange}
+        />
+        <button
+          className="py-1 px-3 bg-green-400 text-white"
+          type="submit"
+          data-test={dataAttrs.button}
+        >
+          GUESS
+        </button>
+      </form>
+    </div>
+  );
 }
+
+// Prop Types
+Input.propTypes = {
+  secretWord: PropTypes.string.isRequired,
+};
 
 export default Input;
