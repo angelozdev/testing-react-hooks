7af1e1d748cad2a69eba58665e0756825634fc64
Angelo Zambrano <angelozam17@gmail.com>
2020-12-06 19:01:05 -0500

    feat: general styles and add tailwind

--- base-for-hooks-context/src/App.js
+++ base-for-hooks-context/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import * as React from "react";
 import "./App.css";
+import Input from "./components/Input";
 
 function App() {
   return (
-    <div data-test="app-container" className="App">
-      <h1>Hola mundo</h1>
+    <div data-test="app-container" className="my-4">
+      <div className="container mx-auto border hover:shadow transition-shadow duration-300 ease">
+        <div className="p-4">
+          <Input secretWord="train" />
+        </div>
+      </div>
     </div>
   );
 }
